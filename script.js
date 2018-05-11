fee7e973f8b76a576c37a5f963e17193f65860a1
Johnson <johnnyjp2070@gmail.com>
2018-05-11 12:36:32 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Using For Loop");
+for (var i = 1; i <= 10; i++){
+    console.log(i * 5);
+}
+console.log("Using While Loop");
+var j = 1;
+while (j <= 10){
+    console.log(j * 5);
+    j++;
+}
